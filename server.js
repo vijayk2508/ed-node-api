c11e1f7504d372430d71eb744f4b275206e51049
vijay kumar <vijay.kumar@emvigotech.com>
2022-04-26 09:04:07 +0530

    update

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const product = require("./api/product");
 app.use('/static', express.static(path.join(__dirname + '/public')));
 
 app.use(express.json({ extended: false }));
+
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
 app.use("/api/product", product);
 
 const PORT = process.env.PORT || 8080;
